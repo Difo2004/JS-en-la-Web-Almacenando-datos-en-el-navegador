e01c4a6836ce4c1973d3e889da11c7a1bb5e98a1
Difo2004 <104111109+Difo2004@users.noreply.github.com>
2022-11-04 09:23:08 -0400

    proyecto completado

--- components/deleteIcon.js
+++ components/deleteIcon.js
@@ -1,13 +1,25 @@
-const deleteIcon = () => {
+import { displayTask } from "./readTasks.js";
+
+const deleteIcon = (id) => {
+
   const i = document.createElement('i');
   i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
-  i.addEventListener('click', deleteTask);
+  i.addEventListener('click', _ => deleteTask(id));
+
   return i;
 };
 
-const deleteTask = (event) => {
-  const parent = event.target.parentElement;
-  parent.remove();
+const deleteTask = (id) => {
+
+  document.querySelector("[data-list]");
+  li.innerHTML = '';
+
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  const index = tasks.findIndex(item => item.id === id);
+  tasks.splice(index, 1);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  displayTask();
 };
 
 export default deleteIcon;
